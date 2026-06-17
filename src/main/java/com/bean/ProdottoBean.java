ab2706d15b12e69f041b56b8bbd3393b979b5335
fraaaannnnn <sannellafrancesco05@gmail.com>
2026-06-17 18:14:30 +0200

    Aggiunto ProdottoBean

---
+++ src/main/java/com/bean/ProdottoBean.java
@@ -0,0 +1,62 @@
+package com.bean;
+
+import java.io.Serializable;
+
+public class ProdottoBean implements Serializable {
+    
+    private static final long serialVersionUID = 1L;
+    
+    private int id;
+    private String nome;
+    private String descrizione;
+    private double prezzo;
+    private String immagine;
+    
+    public ProdottoBean() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescrizione() {
+        return descrizione;
+    }
+
+    public void setDescrizione(String descrizione) {
+        this.descrizione = descrizione;
+    }
+
+    public double getPrezzo() {
+        return prezzo;
+    }
+
+    public void setPrezzo(double prezzo) {
+        this.prezzo = prezzo;
+    }
+
+    public String getImmagine() {
+        return immagine;
+    }
+
+    public void setImmagine(String immagine) {
+        this.immagine = immagine;
+    }
+
+    @Override
+    public String toString() {
+        return "ProdottoBean [id=" + id + ", nome=" + nome + ", prezzo=" + prezzo + "]";
+    }
+}
